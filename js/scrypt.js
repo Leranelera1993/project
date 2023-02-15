74320ba61fc9d11c3cad2f4b1f1060be79616f09
Lera <example@gmail.com>
2023-02-15 21:17:12 +0300

    functions rasshirenii

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,4 +1,14 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+
+//start();
 
 const personalMoovieDB = {
   count: numberOfFilms,
@@ -8,12 +18,52 @@ const personalMoovieDB = {
   private: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов", ""),
-  b = prompt("На сколько его оцените?", ""),
-  c = prompt("Один из последних просмотренных фильмов", ""),
-  d = prompt("На сколько его оцените?", "");
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt("На сколько его оцените?", "");
 
-personalMoovieDB.movies[a] = b;
-personalMoovieDB.movies[c] = d;
+    if (a === null || b === null || a === "" || b === "" || a.length > 50) {
+      console.log("error");
+      i--;
+    } else {
+      personalMoovieDB.movies[a] = b;
+      console.log("done");
+      //if (a !== null && b !== null && a !== "" && b !== "" && a.length < 50) {
+      //personalMoovieDB.movies[a] = b;
+      //console.log("done");
+      //} else {
+      // console.log("error");
+      //i--;
+    }
+  }
+}
+
+//rememberMyFilms();
+
+function detectedPersonalLevel() {
+  if (personalMoovieDB.count < 10) {
+    console.log("nizkii ball");
+  }
+}
+
+//detectedPersonalLevel();
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+    personalMoovieDB.genres[i - 1] = genre;
+  }
+}
+
+writeYourGenres();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMoovieDB);
+  }
+}
+
+//showMyDB(personalMoovieDB.private);
 
 console.log(personalMoovieDB);
